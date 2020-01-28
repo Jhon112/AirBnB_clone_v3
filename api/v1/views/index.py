3a5dab1ee5f4ddd3949af9457f73785bcab085bb
valerc123 <valentina.rua123@gmail.com>
2020-01-28 19:06:06 +0000

    Pep8

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,10 +4,10 @@
 from api.v1.views import app_views
 from flask import jsonify
 
+
 @app_views.route('/status')
 def status():
     """
         Return status `Ok`
     """
     return jsonify(status='OK')
-
