de88d783c3c768fb10e9442d773c8d1df0f141f9
Holberton <holberton@Holbertons-iMac-77.local>
2020-01-28 14:52:15 -0500

    WIP

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,9 @@
 #!/usr/bin/pyhton3
-"""
+""" Views
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import file_storage
 
 
 @app_views.route('/status')
@@ -11,3 +12,11 @@ def status():
         Return status `Ok`
     """
     return jsonify(status='OK')
+
+def stats():
+    """
+        Return the number of each objects
+    """
+    stats = file_storage.count()
+    return jsonify(**stats)
+    
