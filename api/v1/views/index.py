7ce5725e10ef6ce310e2207228ede664a736b98a
valerc123 <valentina.rua123@gmail.com>
2020-01-28 21:27:38 +0000

    return dictionary with the number of each object

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,7 @@
 """
 from api.v1.views import app_views
 from flask import jsonify
-from models.engine import file_storage
+from models import storage
 
 
 @app_views.route('/status')
@@ -13,11 +13,16 @@ def status():
     """
     return jsonify(status='OK')
 
+
 @app_views.route('/stats')
-def stats(self):
+def stats():
     """
         Return the number of each objects
     """
-    stats = self.count()
-    return jsonify(**stats)
-
+    stats = {"amenities": storage.count('Amenity'),
+             "cities": storage.count('City'),
+             "places": storage.count('Place'),
+             "reviews": storage.count('Review'),
+             "states": storage.count('State'),
+             "users": storage.count('User')}
+    return jsonify(stats)
