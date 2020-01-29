998309a759a836b4b55e1468e65217f7f2bd7897
Jhon Arias <jhonarmont@hotmail.com>
2020-01-29 18:06:03 -0500

    fix error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,13 +2,14 @@
 """
 App file
 """
-from flask import Flask, Blueprint, jsonify
+from flask import Flask, Blueprint, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
@@ -25,7 +26,7 @@ def page_not_found(error):
         handle error 404
     """
     error = {"error": "Not found"}
-    return jsonify(error)
+    return make_response(jsonify(error), 404)
 
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST')
