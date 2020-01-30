54b1e1f954db672ff067f123b43b72bd6e4882e7
valerc123 <valentina.rua123@gmail.com>
2020-01-30 05:28:22 +0000

    add strict_slashes at routes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,7 +6,7 @@ from flask import jsonify
 from models import storage
 
 
-@app_views.route('/status')
+@app_views.route('/status', strict_slashes=False)
 def status():
     """
         Return status `Ok`
@@ -14,7 +14,7 @@ def status():
     return jsonify(status='OK')
 
 
-@app_views.route('/stats')
+@app_views.route('/stats', strict_slashes=False)
 def stats():
     """
         Return the number of each objects
